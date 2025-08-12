8c7f49cdeec00958eec245e31a3aa4215f61c13f
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2025-08-12 08:13:47 +0530

    Remove expo-gl dependencies and simplify metro config to fix build issues

--- metro.config.js
+++ metro.config.js
@@ -2,8 +2,4 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Disable Watchman to fix FSEvents issues
-config.watchFolders = [];
-config.resolver.useWatchman = false;
-
 module.exports = config; 
